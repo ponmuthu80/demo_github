2fe5c814702727ab3caba1c7a12994c8b206c989
ponmuthu80 <pons.muthu@gmail.com>
2024-11-10 11:49:47 -0500

    Add boys and girls calculation

--- python1.py
+++ python1.py
@@ -2,3 +2,8 @@ print("Hello Python")
 a="welcome"
 b="to python world"
 print(a,b)
+a=20
+b=10
+print("no of boys in class:=",a)
+print("no of girls in class:=",b)
+print("Total no of students in class:=",a+b)
