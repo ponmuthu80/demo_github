7d7137b72691fcdf722184a814d42dbc6b100edc
ponmuthu80 <pons.muthu@gmail.com>
2024-11-10 12:48:54 -0500

    Python2 Version 1

---
+++ Python2.py
@@ -0,0 +1,11 @@
+a=10
+b=5
+print("Product of a and b:=",a*b)
+print("Sum of a and b:=",a+b)
+print("Subtract of a from b:=",a-b)
+print("Division of a by b:=",a/b)
+if a > b:
+    print("a is big",a)
+else:
+    print("b is big",b)
+
